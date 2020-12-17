66a1b4b9d91b6212d647b6a30d8b197163f599a9
OliverWales <oliverfwales@gmail.com>
2020-12-17 10:58:42 +0000

    Add comments

--- source/main.s
+++ source/main.s
@@ -2,17 +2,35 @@
 .globl _start
 _start:
 
+/* 
+* Address of GPIO region
+*/
 ldr r0,=0x20200000
 
+/* 
+* Set bits 18-20 of r1 to 001
+*/
 mov r1,#1
 lsl r1,#18
 
+/* 
+* Set the GPIO function select
+*/
 str r1,[r0,#4]
 
+/* 
+* Set the 16th bit of r1
+*/
 mov r1,#1
 lsl r1,#16
 
+/* 
+* Set GPIO 16 to low
+*/
 str r1,[r0,#40]
 
+/*
+* Loop forever
+*/
 loop$:
 b loop$
