56fb0ac797484996128a76072a3c2634ae9c9f48
OliverWales <oliverfwales@gmail.com>
2020-12-19 09:04:03 +0000

    Implement lesson 5

--- source/main.s
+++ source/main.s
@@ -16,37 +16,44 @@ main:
     .unreq pinNum
     .unreq pinFunc
 
+    /* Load pattern into r4 */
+    ptrn .req r4
+    ldr ptrn,=pattern
+    ldr ptrn,[ptrn]
+
+    /* Index into pattern stored in r5 */
+    seq .req r5
+    mov seq,#0
+
     loop:
-        /* Set pin 16 low (LED on) */
-        pinNum .req r0
+        /* Get current value in pattern */
         pinVal .req r1
+        mov r1,#1
+        lsl r1,seq
+        and r1,ptrn
+
+        /* Set pin 16 to that value */
+        pinNum .req r0
         mov pinNum,#16
         mov pinVal,#0
         bl SetGpio
         .unreq pinNum
         .unreq pinVal
 
-        /* Wait 0.5 seconds */
+        /* Wait 0.25 seconds */
         duration .req r0
-        ldr duration,=500000
+        ldr duration,=250000
         bl Wait
         .unreq duration
 
-        /* Set pin 16 high (LED off) */
-        pinNum .req r0
-        pinVal .req r1
-        mov pinNum,#16
-        mov pinVal,#1
-        bl SetGpio
-        .unreq pinNum
-        .unreq pinVal
-
-        /* Wait 0.5 seconds */
-        duration .req r0
-        ldr duration,=500000
-        bl Wait
-        .unreq duration
+        /* Increment index and reset if >= 32 */
+        add seq,#1
+        and seq,#0b11111
 
         /* Loop forever */
         b loop
 
+.section .data
+.align 2
+pattern:
+    .int 0b11111111101010100010001000101010
