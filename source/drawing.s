c757a349301a208b107a6c700f83ba323ca02699
OliverWales <oliverfwales@gmail.com>
2020-12-31 10:15:53 +0000

    Implement line wrapping

--- source/drawing.s
+++ source/drawing.s
@@ -292,6 +292,11 @@ DrawString:
         .unreq tabs
         beq stringLoop$
 
+        /* If x > screen width - width of char, wrap to next line */
+        cmp x,#1018
+        movhi x,x0
+        addhi y,#16
+        
         /* Draw character */
         mov r1,x
         mov r2,y
