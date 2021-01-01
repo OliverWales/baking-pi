e2fc0b92a973db2cc805a4df1187cdd2682f9ca5
OliverWales <oliverfwales@gmail.com>
2021-01-01 17:34:02 +0000

    Tidy up random number function

--- source/random.s
+++ source/random.s
@@ -1,15 +1,17 @@
+/*
+* Return a pseudorandom number seeded by the value in r0
+*/
 .globl Random
 Random:
-xnm .req r0
-a .req r1
+    xnm .req r0
+    a .req r1
 
-mov a,#0xef00
-mul a,xnm
-mul a,xnm
-add a,xnm
-.unreq xnm
-add r0,a,#73
+    mov a,#0xef00
+    mul a,xnm
+    mul a,xnm
+    add a,xnm
+    .unreq xnm
+    add r0,a,#73
 
-.unreq a
-mov pc,lr
-
+    .unreq a
+    mov pc,lr
